f9b47dc73dbc1ce0c4d4bd83389b60eb9e38a35d
ikefix <omekeikechukwu877@gmail.com>
2026-05-19 12:33:22 +0100

    pulling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -162,12 +162,12 @@
         <!-- Logo -->
         <div class="text-center mb-4">
             <h3 class="fw-bold">
-                <span style="color:#6f42c1;"> Bloom Mmonie</span>
+                <span style="color:#6f42c1;"> BloomMonnie</span>
             </h3>
         </div>
 
         <!-- Title -->
-        <h4 class="text-center mb-4 fw-semibold">Welcome Back 👋</h4>
+        <h4 class="text-center mb-4 fw-semibold">Welcome Back </h4>
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
