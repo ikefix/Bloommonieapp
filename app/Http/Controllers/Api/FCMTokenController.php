15d86a33bc03a525a6b61e7baa47eaaeb674d836
ottohcharleswills <ottohcharleswills@gmail.com>
2026-06-28 19:01:53 +0100

    testin

--- app/Http/Controllers/Api/FCMTokenController.php
+++ app/Http/Controllers/Api/FCMTokenController.php
@@ -24,10 +24,24 @@ class FCMTokenController extends Controller
     //     ]);
     // }
 
-    public function saveFcmToken(Request $request)
-    {
-        $request->validate(['fcm_token' => 'required|string']);
-        $request->user()->update(['fcm_token' => $request->fcm_token]);
-        return response()->json(['status' => true, 'message' => 'FCM token saved']);
-    }
+public function saveFcmToken(Request $request)
+{
+    $request->validate([
+        'fcm_token' => 'required|string',
+    ]);
+
+    $user = $request->user();
+
+    logger()->info([
+        'user_id' => optional($user)->id,
+        'token_length' => strlen($request->fcm_token),
+    ]);
+
+    $user->fcm_token = $request->fcm_token;
+    $user->save();
+
+    return response()->json([
+        'status' => true,
+    ]);
+}
 }
