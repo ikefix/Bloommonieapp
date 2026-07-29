82be0119af92048513ad91c1317d3e03556d659a
ikefix.git <omekeikechukwu877@gmail.com>
2026-07-29 08:22:46 -0700

    untouch

--- resources/views/products/partials/table.blade.php
+++ resources/views/products/partials/table.blade.php
@@ -24,32 +24,6 @@
                 <td>{{ $product->stock_quantity }}</td>
                 <td>{{ $product->shop->name ?? 'Not assigned' }}</td>
                 <td>{{ $product->stock_unit ?? 'Not assigned' }}</td>
-                 <!-- <td>
-
-                    {{ $product->stock_unit ?? 'Not assigned' }}
-
-                    @if($product->stock_unit && $product->unit_size)
-
-                        @php
-                            $remainingUnits = floor(
-                                $product->stock_quantity /
-                                $product->unit_size
-                            );
-                        @endphp
-
-                        <br>
-
-                        <span class="badge bg-info mt-1">
-
-                            {{ number_format($remainingUnits) }}
-                            {{ Str::plural($product->stock_unit, $remainingUnits) }}
-                            Left
-
-                        </span>
-
-                    @endif
-
-                </td> -->
                 <td>{{ $product->unit_size ?? 'Not assigned' }}</td>
                 <td class="product-btn">
                     <button type="button" class="btn btn-sm btn-warning edit-btn"
