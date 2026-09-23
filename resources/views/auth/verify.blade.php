d5883ee574d9ca549ae21ac564e97539bd65f531
ikefix.git <omekeikechukwu877@gmail.com>
2026-09-23 10:18:39 -0700

    bold

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -367,7 +367,7 @@
                 );
 
                 setTimeout(function () {
-                    window.location.href = '/admin/dashboard';
+                    window.location.href = '/admin-dashboard';
                 }, 800);
 
                 return;
